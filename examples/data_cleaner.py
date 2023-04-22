f91370fcc0968742af232b1b1b83d8bcd761dbce
ThomasHeinThura <alexzander125.alex@gmail.com>
2023-04-22 18:52:11 +0630

    add data_cleaner

--- examples/data_cleaner.py
+++ examples/data_cleaner.py
@@ -8,6 +8,9 @@ logger = logging.getLogger(__name__)
 import yaml
 import argparse
 import pandas as pd 
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
+from params_loader import read_params
 
 
 def load_data(data_path):
@@ -17,12 +20,11 @@ def load_data(data_path):
     output:pandas dataframe 
     note: Only 6 variables are used in this model building stage for the simplicity.
     """
+    print("........ Loading Data..........")
     df = pd.read_csv(data_path, sep=",", encoding='utf-8')
     return df
 
-#Check skewness
-
-
+# ------------------------------------------------------------------------------- #
 def check_duplication(raw_dataset):
     #check duplication of data
     number_of_duplications = raw_dataset.duplicated().sum()
@@ -36,8 +38,9 @@ def remove_duplication(raw_dataset):
     else :
         raw_dataset.drop_duplicates(keep=False,inplace=True)
         remove_duplication(raw_dataset)
-    
+# ------------------------------------------------------------------------------- #
 def check_skewness(raw_dataset):
+    #Check skewness
     # this can check relation between each column
     skew_limit=0.75
     skew_value=raw_dataset[raw_dataset.columns].skew()
@@ -47,77 +50,79 @@ def check_skewness(raw_dataset):
     return cols
 
 def powertransform_skewness(raw_dataset):
+    print("Remove duplication")
+    remove_dupli_raw_dataset = remove_duplication(raw_dataset)
+    print("Check skewness")
     skewed_col_nm = check_skewness(raw_dataset)
-    print(f"Skewness col are : \n {skewed_col_nm}")
+    # print(f"Skewness col are : \n {skewed_col_nm}")
     from sklearn.preprocessing import PowerTransformer
     pt=PowerTransformer(standardize=False)
     raw_dataset[skewed_col_nm]=pt.fit_transform(raw_dataset[skewed_col_nm])
-
-def standardize_dataset(raw):
-    return 
-
-def get_feat_and_target(df,target):
+# ------------------------------------------------------------------------------- #
+def get_feat_and_target(raw_dataset,target):
     """
     Get features and target variables seperately from given dataframe and target 
     input: dataframe and target column
     output: two dataframes for x and y 
     """
-    x=df.drop(target,axis=1)
-    y=df[[target]]
-    return x,y   
+    powertransform_dataset = powertransform_skewness(raw_dataset)
+    X=raw_dataset.drop(target,axis=1)
+    y=raw_dataset[target]
+    print("spliiting X and y")
+    return X,y   
+
+def standardize_feature_labels(raw_dataset,target):
+    X,y = get_feat_and_target(raw_dataset, target)
+    #Normalize features
+    sc=StandardScaler()
+    scaled_X=sc.fit_transform(X)
+    print("Standardizating")
+    #Label Encode label
+    lab = preprocessing.LabelEncoder()
+    y_transformed = lab.fit_transform(y)
+    return scaled_X, y_transformed
+# ------------------------------------------------------------------------------- #
+def change_to_pandas(raw_dataset,scaled_X, y_transformed, target):
+    # change feature to pandas
+    print("Reframe to Pandas")
+    data_columns = raw_dataset.drop(target,axis=1)
+    scaled_X = pd.DataFrame(scaled_X, columns=data_columns.columns)
+    # change labels to pandas
+    y_transformed = pd.DataFrame(y_transformed)
+    y_transformed.columns = ['Class']
+    # zip two dataset.
+    clean_dataset =pd.concat([scaled_X, y_transformed],ignore_index=False,axis=1,sort=False)
+    return clean_dataset
+
+def save_clean_data(raw_dataset,target, clean_data_path):
+    scaled_X, y_transformed = standardize_feature_labels(raw_dataset, target)
+    #clean raw dataset and save clean data
+    clean_dataset = change_to_pandas(raw_dataset, scaled_X, y_transformed, target)
+    print("Finish cleaning and save to path.")
+    clean_dataset.to_csv(clean_data_path,index=False)
 
 def clean_raw_data(config_path):
     """
-        These are 
+        cleaning the dataset 
+        selecting the features and labels
+        Normalize the dataset
+        clean duplication
     """
     # take parameters from config file
     config=read_params(config_path)
     raw_data_path=config["raw_data_config"]["raw_data_csv"]
     clean_data_path=config["clean_data_config"]["clean_data_csv"]
+    target = config["train_test_config"]["target"]
     
     # load data
     creditcard_df=load_data(raw_data_path)
-    creditcard_df=remove_duplication(creditcard_df)
-    powertransform_skewness(creditcard_df)
-    
-    
-    
-    
-    
-    #clean raw dataset and save clean data
-    clean_data.to_csv(raw_data_path,index=False)
+    print("START CLEANING")
+    save_clean_data(creditcard_df, target, clean_data_path)
+    print("Finish all processes")
+
     
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
-    load_raw_data(config_path=parsed_args.config)
-
-
-
-
-
-
-
-
-# Feature and label
-X = creditcard_df.drop(['Class'], axis=1)
-y = creditcard_df['Class']
-y.value_counts().to_frame()
-
-
-from sklearn.preprocessing import StandardScaler
-### Note that you can fit_transform the whole oversampled features(train+test) from begining
-sc=StandardScaler()
-scaled_X=sc.fit_transform(X)
-scaled_X
-
-from sklearn import preprocessing
-from sklearn import utils
-lab = preprocessing.LabelEncoder()
-y_transformed = lab.fit_transform(y)
-
-print(scaled_X.shape, y.shape)
-
-### output transform dataset
-
+    clean_raw_data(config_path=parsed_args.config)
